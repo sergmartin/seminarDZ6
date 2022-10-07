2633f0c7cd2c48a7ed79210a576515752f204942
Сергей <sergemartin@yandex.ru>
2022-10-07 22:37:11 +0300

    Решил задачу 43

---
+++ task43/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 43
+Console.Clear();
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2*x + b2;
+
+Console.Write($" Прямые пересекуться в точках:({x}, {y})");
